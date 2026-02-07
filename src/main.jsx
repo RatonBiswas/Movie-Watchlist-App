6bc112506f92f45e0b2ab39cb0af468673df2bb4
RatonBiswas <raton35-2241@diu.edu.bd>
2026-02-08 01:01:15 +0600

    Add react-hot-toast and mount toaster

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { AuthProvider } from "./services/auth.jsx";
 import { WatchlistProvider } from "./services/watchlist.jsx";
+import { Toaster } from "react-hot-toast";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -12,6 +13,19 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <AuthProvider>
         <WatchlistProvider>
           <App />
+          <Toaster
+            position="top-center"
+            containerStyle={{ zIndex: 99999, top: 80 }}
+            toastOptions={{
+              duration: 2500,
+              style: {
+                background: "#111827",
+                color: "#fff",
+                borderRadius: "14px",
+                padding: "10px 14px"
+              }
+            }}
+          />
         </WatchlistProvider>
       </AuthProvider>
     </BrowserRouter>
